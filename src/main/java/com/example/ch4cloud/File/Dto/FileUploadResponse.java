7be7b66dd57a55b0fc21359805342c10bacf30bf
jy030506@naver.com <jy030506@naver.com>
2026-05-19 20:10:01 +0900

    Lv3 : S3 실습 파일 작성

---
+++ src/main/java/com/example/ch4cloud/File/Dto/FileUploadResponse.java
@@ -0,0 +1,14 @@
+package com.example.ch4cloud.File.Dto;
+
+import lombok.Getter;
+
+@Getter
+public class FileUploadResponse {
+
+    private final String key;
+
+
+    public FileUploadResponse(String key) {
+        this.key = key;
+    }
+}
